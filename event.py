1476121461b268ddd89a178b4f8631669b242a4c
Shipra Shalini <code.shipra@gmail.com>
2021-02-19 21:21:43 +0530

    reduces complexity of on_user_message

--- event.py
+++ event.py
@@ -25,26 +25,15 @@ def update_current_leaderboard_page_number(page):
     current_leaderboard_page_number = page
 
 
-def get_prompt_help():
-    return discord.Embed(
-        title="DN Bot Guide",
-        description=(
-            "DN Bot is especially designed for the users of Devsnest Community. "
-            "DN bot is always there to help and make your learning fun. "
-            "Use the following commands for a smooth experience on the platform:\n"
-        ),
-    ).set_thumbnail(url="https://cdn.wayscript.com/blog_img/83/DiscordBotThumb.png")
-
-
-async def on_user_message(message):
-    if message.content.startswith("dn-assign-mentors"):
+class UserMessageHandler:
+    def process_dn_assign_mentors(self, message):
         asyncio.ensure_future(assign_mentors_to_all())
 
-    if message.content.startswith("dn-hello"):
+    def process_dn_hello(self, message):
         msg = f"Hello {message.author.mention}!"
         asyncio.ensure_future(message.channel.send(msg))
 
-    if message.content.startswith("dn-help"):
+    def process_dn_help(self, message):
         msg = (
             "dn-help: To get command help.\n\n"
             "dn-fetch: To get list of questions.\n\n"
@@ -55,44 +44,44 @@ async def on_user_message(message):
             "dn-leaderboard: To get list of top 10 students of week.\n"
         )
 
-        prompt = get_prompt_help()
+        prompt = self._get_prompt_help()
         prompt.add_field(
             name="Here is your ultimate guide to DN bot.\n", value=msg, inline=False
         )
 
         asyncio.ensure_future(message.channel.send(embed=prompt))
 
-    if message.content.startswith("dn-email"):
+    def process_dn_email(self, message):
         user_email = await get_user_email_and_id(message.author)
         if user_email:
             asyncio.ensure_future(submit_user_details(message.author, user_email))
 
-    if message.content.startswith("dn-fetch"):
-        if await check_channel_ask_a_bot(message):
+    def process_dn_fetch(self, message):
+        if await check_channel_ask_a_bot(self, message):
             asyncio.ensure_future(fetch(message))
 
-    if message.content.startswith("dn-mark-done"):
-        if await check_channel_ask_a_bot(message):
+    def process_dn_mark_done(self, message):
+        if await check_channel_ask_a_bot(self, message):
             asyncio.ensure_future(mark_ques_status(message.author, message, 0))
 
-    if message.content.startswith("dn-mark-undone"):
-        if await check_channel_ask_a_bot(message):
+    def process_dn_mark_undone(self, message):
+        if await check_channel_ask_a_bot(self, message):
             asyncio.ensure_future(mark_ques_status(message.author, message, 1))
 
-    if message.content.startswith("dn-mark-doubt"):
-        if await check_channel_ask_a_bot(message):
+    def process_dn_doubt(self, message):
+        if await check_channel_ask_a_bot(self, message):
             asyncio.ensure_future(mark_ques_status(message.author, message, 2))
 
-    if message.content.startswith("dn-report"):
-        if await check_channel_ask_a_bot(message):
+    def process_dn_report(self, message):
+        if await check_channel_ask_a_bot(self, message):
             days = await calc_days(message)
             if days:
                 resp = await get_report_from_db(message, days)
                 # ToDo report handling
                 asyncio.ensure_future(show_user_report(resp, message, days))
 
-    if message.content.startswith("dn-leaderboard"):
-        if await check_channel_ask_a_bot(message):
+    def process_dn_leaderboard(self, message):
+        if await check_channel_ask_a_bot(self, message):
             global current_leaderboard_page_number
             global last_leaderboard_message_id
 
@@ -104,3 +93,20 @@ async def on_user_message(message):
             last_leaderboard_message_id = await get_leaderboard(
                 message, current_leaderboard_page_number
             )
+
+    def _get_prompt_help():
+        return discord.Embed(
+            title="DN Bot Guide",
+            description=(
+                "DN Bot is especially designed for the users of Devsnest Community. "
+                "DN bot is always there to help and make your learning fun. "
+                "Use the following commands for a smooth experience on the platform:\n"
+            ),
+        ).set_thumbnail(url="https://cdn.wayscript.com/blog_img/83/DiscordBotThumb.png")
+
+
+async def on_user_message(self, message):
+    command = message.split(" ", 1)[0].replace("-", "_")
+    method_name = f"process_{command}"
+    method = getattr(UserMessageHandler(), method_name)
+    method(message)
