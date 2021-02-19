e17cc773882b8c58bce053a49ecbb2c07745b740
Shipra Shalini <code.shipra@gmail.com>
2021-02-19 21:47:20 +0530

    some minor message changes

--- services/content.py
+++ services/content.py
@@ -13,12 +13,12 @@ load_dotenv()
 total_leaderboard_pages = 100
 
 STATUS_DESC_MAP = {
-    0: "Congratulations‼ \nThis question has been marked as done. Keep Going 😄",
+    0: "Congratulations‼ \nThis question has been marked as done. Keep Going.. 😄",
     1: "Hey, This question has been marked as undone. Try solving it. All the best‼ 😎",
     2: (
-        "Seems like, you're Stuck‼ 😶\nThis question has been marked as doubt. "
-        "Give it a try, in case you are not able to solve, "
-        "feel free to contact your mentor. Let this not hinder your learning 👍"
+        "Seems like you're stuck‼ 😶\nThis question has been marked as doubt. "
+        "Give it a try, in case you are not able to solve it , "
+        "feel free to contact your mentor. Let this not hinder your learning. 👍"
     ),
 }
 
